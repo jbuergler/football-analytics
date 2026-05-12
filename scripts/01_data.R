4f5106b8454d0f622ef379557f66f918011207ec
Buergler <hsjbuerg@liverpool.ac.uk>
2026-05-12 15:43:04 +0100

    added page number in reference in 01data

--- scripts/01_data.R
+++ scripts/01_data.R
@@ -38,7 +38,7 @@ weuro_matches %>%
 
 
 # --- 4. Pull events for each match and combine into one dataframe ----
-# according to StatsBomb Working with R guide (StatsBomb, 2022).
+# according to StatsBomb Working with R guide (StatsBomb, 2022, p.11).
 # https://blogarchive.statsbomb.com/uploads/2022/08/Working-with-R.pdf
 events_raw <- free_allevents(MatchesDF = weuro_matches, Parallel = TRUE)
 
