4d70c28ccadd41d8254541929508a73691523799
Buergler <hsjbuerg@liverpool.ac.uk>
2026-05-12 12:47:43 +0100

    update 00_setup.R to include GitHub installs for StatsBombR

--- scripts/00-setup.R
+++ scripts/00-setup.R
@@ -5,17 +5,19 @@
 # This project was built on:
 # R Version: R version 4.5.1 (2025-06-13)
 
-library(tidyverse)
-library(StatsBombR)
-library(naniar)
-library(ggrepel)
-library(ggsoccer)
-library(gt)
-library(bslib)
-library(plotly)
-library(shiny)
-library(bsicons)
-
+install.packages("devtools") # needed for statsbomb
+devtools::install_github("statsbomb/StatsBombR") # have to install StatsBomb package first
+library(StatsBombR) # StatsBomb open event data
+library(tidyverse) # data wrangling and visualisation (ggplot)
+library(naniar) # missing variables
+library(ggrepel) # non-overlapping labels in ggplot
+library(ggsoccer) # football pitch plot
+library(gt) # formatted tables
+library(bslib) # shiny app theme
+library(bsicons) # icons for shiny UI
+library(plotly) # interactive charts for dashboard
+library(shiny) # shiny app framework
+library(shinylive) # deploy shiny app via WebAssembly
 
 
 
